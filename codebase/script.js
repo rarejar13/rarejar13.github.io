4ab9b6f5d74eccdbcd3aab5e6bf8493bdd87b5bc
rarejar13 <rareatra@gmail.com>
2020-02-20 06:27:17 +0200

    Update script.js

--- codebase/script.js
+++ codebase/script.js
@@ -108,13 +108,7 @@ webix.ui({
         }
       }
     },
-
-
-
-
-
-                    
-                    ]},//header
+         ]},//header
 
 
         {cols:[
@@ -131,12 +125,23 @@ webix.ui({
                         {value:"Location"}
                     ]},//sidebar
 
-
-
-            {template:"Table"},
-            
+            {view:"datatable", id:"grid",
+              columns:[
+                      { id:"rank",  editor:"text",  header:"№", css:"rank",  width:50},
+                      { id:"title", editor:"text",  header:"Имя", fillspace:true},
+                      { id:"year",  editor:"text",  header:"Адрес" ,  width:400},
+                      { id:"votes", editor:"text",  header:"Номер телефона",    width:300},
+                      { id:"votes", editor:"text",  header:"Дата",    width:200}
+                      ],
+              scrollX:false,
+              autoConfig:true,
+              select:"row", editable:true, editaction:"dblclick",
+              url: "/src/data.php",// your custom script for loading
+              save:{
+              url:"/src/save.php",// your custom script for saving
+              autoupdate:false
+              }}, 
             ]},//table
-
 
         {cols:[{height: 30, template:"homenet.it", css:"center_text"}]}//footer
     ]
